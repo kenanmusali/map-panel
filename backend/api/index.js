e8197a22c4f90c6296e363eb93931a2ee4e402dc
kenanmusali <kenan_musali@outlook.com>
2026-05-26 14:31:39 +0400

    updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,1 @@
-// Vercel Serverless entry point
-// Uses dynamic import because backend has "type": "module" (ESM)
-let app;
-
-module.exports = async (req, res) => {
-  if (!app) {
-    const mod = await import('../server.js');
-    app = mod.default;
-  }
-  app(req, res);
-};
+export { default } from '../server.js';
